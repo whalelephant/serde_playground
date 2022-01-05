85282334151ee3a89d4d61670ea4cc1b2ebcf537
bwty <whalelephant@users.noreply.github.com>
2022-01-05 09:52:19 +0100

    use RawValue

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use num_bigint::BigUint;
 use serde::de;
 use serde::{
-    de::{DeserializeSeed, Error, Visitor},
+    de::{DeserializeSeed, Visitor},
     Deserialize,
 };
 use std::fs::File;
+use std::io::{BufReader, Read};
 use std::path::Path;
+use std::str::FromStr;
 
 #[derive(Debug)]
+struct DeBigUintType(BigUint);
+
+#[derive(Debug, Deserialize)]
 struct Input {
-    pub p: Box<BigUint>,
-    pub q: Box<BigUint>,
+    pub p: DeBigUintType,
+    pub q: DeBigUintType,
 }
 
 #[derive(Deserialize)]
@@ -20,97 +25,30 @@ enum Fields {
     Q,
 }
 
-pub struct FromStringVisitor;
-
-impl<'de> DeserializeSeed<'de> for FromStringVisitor {
-    type Value = String;
-
-    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        deserializer.deserialize_str(self)
-    }
-}
-
-impl<'de> Visitor<'de> for FromStringVisitor {
-    type Value = String;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("expects a string")
-    }
-
-    fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        self.visit_string(s.to_owned())
-    }
-
-    fn visit_string<E>(self, s: String) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        Ok(s)
-    }
-}
-
-impl<'de> Deserialize<'de> for Input {
+impl<'de> Deserialize<'de> for DeBigUintType {
     fn deserialize<D>(deserializer: D) -> Result<Self, <D as serde::Deserializer<'de>>::Error>
     where
         D: serde::Deserializer<'de>,
     {
-        struct InputVisitor;
+        let raw_value: &serde_json::value::RawValue =
+            <&serde_json::value::RawValue>::deserialize(deserializer).map_err(|e| {
+                println! {"helloo erro"};
+                e
+            })?;
+        let raw_value = raw_value.get();
 
-        impl<'de> Visitor<'de> for InputVisitor {
-            type Value = Input;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("expected either string or int from json")
-            }
-
-            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-            where
-                A: serde::de::MapAccess<'de>,
-            {
-                let mut p = None;
-                let mut q = None;
-                while let Some(key) = map.next_key()? {
-                    match key {
-                        Fields::P => {
-                            if p.is_some() {
-                                return Err(de::Error::duplicate_field("p"));
-                            }
-                            let v: String = map.next_value_seed(FromStringVisitor)?;
-                            p = Some(BigUint::parse_bytes(v.as_bytes(), 10).unwrap())
-                        }
-                        Fields::Q => {
-                            if q.is_some() {
-                                return Err(de::Error::duplicate_field("q"));
-                            }
-                            let v: String = map.next_value_seed(FromStringVisitor)?;
-                            q = Some(BigUint::parse_bytes(v.as_bytes(), 10).unwrap())
-                        }
-                    }
-                }
-                let p = p.ok_or_else(|| de::Error::missing_field("p"))?;
-                let q = q.ok_or_else(|| de::Error::missing_field("q"))?;
-
-                Ok(Input {
-                    p: Box::new(p),
-                    q: Box::new(q),
-                })
-            }
-        }
-
-        const FIELDS: &'static [&'static str] = &["p", "q"];
-        deserializer.deserialize_struct("Input", FIELDS, InputVisitor)
+        Ok(DeBigUintType(
+            BigUint::from_str(raw_value).map_err(de::Error::custom)?,
+        ))
     }
 }
 
 fn main() {
     let json_file_path = Path::new("./input.json");
     let file = File::open(json_file_path).expect("file not found");
-    let input: Input = serde_json::from_reader(file).expect("error while reading");
-    println!("input: {:?}", input);
+    let mut r = BufReader::new(file);
+    let mut input = Vec::new();
+    r.read_to_end(&mut input).unwrap();
+    let re: Input = serde_json::from_slice(&input).unwrap();
+    println!("input: {:?}", re);
 }
